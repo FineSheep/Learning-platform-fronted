24471af4e4e2a388c3583f91af22e33780a32588
yang <2804821936@qq.com>
2023-01-01 17:30:58 +0800

    整理首页

--- learning-platform-fronted/src/components/case/ListCard.vue
+++ learning-platform-fronted/src/components/case/ListCard.vue
@@ -1,10 +1,9 @@
 <template>
-    <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
-        <div slot="footer"><b>ant design vue</b> footer part</div>
+    <a-list item-layout="vertical" size="large"  :data-source="listData">
         <a-list-item slot="renderItem" key="item.title" slot-scope="item, index">
             <template v-for="{ type, text } in actions" slot="actions">
         <span :key="type">
-          <a-icon :type="type" style="margin-right: 8px" />
+          <a-icon :type="type" style="margin-right: 8px"/>
           {{ text }}
         </span>
             </template>
@@ -14,20 +13,21 @@
                     alt="logo"
                     src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
             />
-            <a-list-item-meta :description="item.description">
-                <a slot="title" :href="item.href">{{ item.title }}</a>
+            <a-list-item-meta :description="item.putTime">
+                <a slot="title">{{ item.title }}</a>
                 <a-avatar slot="avatar" :src="item.avatar"/>
             </a-list-item-meta>
             <div class="box">
                 {{ item.content }}
             </div>
-
         </a-list-item>
     </a-list>
 </template>
 <script>
+    import myAxios from "@/axios/myAxios";
+
     const listData = [];
-    for (let i = 0; i < 23; i++) {
+/*    for (let i = 0; i < 23; i++) {
         listData.push({
             href: 'https://www.antdv.com/',
             title: `ant design vue part ${i}`,
@@ -40,19 +40,19 @@
                 '你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好' +
                 '你好你好你好你好你好你好你好你好你好你好',
         });
-    }
+    }*/
 
     export default {
         name: "ListCard",
         data() {
             return {
                 listData,
-                pagination: {
+/*                pagination: {
                     onChange: page => {
                         console.log(page);
                     },
                     pageSize: 3,
-                },
+                },*/
                 actions: [
                     {type: 'star-o', text: '156'},
                     {type: 'like-o', text: '156'},
@@ -60,6 +60,23 @@
                 ],
             };
         },
+        async mounted() {
+            const post = await myAxios.get(`/information/getInfo?curPage=${1}&pageSize=10`);
+            const records = post.data.records;
+            console.log(records)
+            for (let r in records) {
+                console.log(records[r])
+                listData.push({
+                    id: records[r].id,
+                    title: records[r].title,
+                    putTime: records[r].putTime,
+                    source: records[r].source,
+                    content: records[r].content,
+                    link: records[r].link,
+                    photo: records[r].photo
+                })
+            }
+        }
     };
 </script>
 <style scoped>
